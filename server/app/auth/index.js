b6d972d95cf699fbc8aadfa84b6de4ac4d024f5b
Moymat <deckertmichel@gmail.com>
2021-11-02 10:54:27 +0100

    test

--- server/app/auth/index.js
+++ server/app/auth/index.js
@@ -29,7 +29,7 @@ const isAuth = async (req, res, next) => {
 	const refresh = req.headers.authorization?.split("Bearer ")[1];
 	const { access_token: token } = req.cookies;
 
-	console.log(req.cookies);
+	console.log(token);
 
 	let decodedRefresh;
 	const notLoggedInError = new Error("not logged in");
