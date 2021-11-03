cd752435605a526d70cb798ea32f71c01a47297e
Moymat <deckertmichel@gmail.com>
2021-11-03 19:49:03 +0100

    add give_viewer_role and fix on get_users_bristols, move_bristol

--- client/src/redux/middlewares/index.js
+++ client/src/redux/middlewares/index.js
@@ -1,16 +1,35 @@
-//import axios from "axios";
+import axios from "../../utils/axios";
+import { createNestedMenu } from "../../components/Bristol/BristolTree/helper";
 
 const Middleware = store => next => action => {
-	//console.log(action)
-
-	// pour que l'action arrive au reducer, je dois utiliser next :
-	next(action);
+	switch (action.type) {
+		case "MOVE_BRISTOL":
+			axios()
+				.post("/api/v1/bristols/move", {
+					bristol_id: action.id,
+					parent_id: action.parent_id,
+					position: action.position,
+				})
+				.then(({ data }) => {
+					console.log(data);
+					next(action);
+				})
+				.catch(err => console.error("middleware", err));
+			break;
+		case "SET_BRISTOLS":
+			axios()
+				.get(`/api/v1/users/${action.userId}/bristols`)
+				.then(({ data }) => {
+					console.log(data.data);
+					action.bristols = createNestedMenu(data.data);
+					next(action);
+				})
+				.catch(err => console.error(err));
+			break;
+		default:
+			next(action);
+			break;
+	}
 };
 
 export default Middleware;
-
-/* const logout = async () => {
-    await axios.get("/auth/logout");
-    localStorage.removeItem("refresh_token");
-    setUser({});
-}; */
