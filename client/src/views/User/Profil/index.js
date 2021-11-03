f52a922ebe4f963cade88719e31f3b73da3ea31d
TonyDaii <jicstar94@gmail.com>
2021-11-03 15:09:53 +0100

    Creation de la vue settings et changement dynamique pour le profillayout

--- client/src/views/User/Profil/index.js
+++ client/src/views/User/Profil/index.js
@@ -1,77 +1,112 @@
 import Box from "@mui/material/Box";
 import { TextField, Button } from "@mui/material";
+import Typography from "@mui/material/Box";
 import "./style.scss";
+import { UserContext } from "../../../App";
+import { useContext, useState } from "react";
 
 export default function Profil() {
-	return (
-		<Box
-			component="form"
-			sx={{
-				"& .MuiTextField-root": { m: 2, width: "25ch" },
-			}}>
-			<h1 className="form-title">Profil</h1>
-			<Box>
-				<p className="form-description">My profil :</p>
+	const {user} = useContext(UserContext);
+	const [users, setUsers] = useState({
+		firstName: user.first_name,
+		lastName: user.last_name,
+		userDescription: "",
+	});
+	
+	const handleChange = (event) => {
+		const { name, value } = event.target;
+		console.log(name)
+		setUsers({
+			...users,
+			[name]: value, 
+		});
+	};
 
-				<div className="form-field">
-					<p>First Name :</p>
-					<TextField
-						id="outlined-basic"
-						name="FirstName"
-						label="First Name"
-						variant="outlined"
-					/>
-				</div>
-				<div className="form-field">
-					<p>Last Name :</p>
-					<TextField
-						id="outlined-basic"
-						name="LastName"
-						label="Last Name"
-						variant="outlined"
-					/>
-				</div>
-			</Box>
-			<Box
-				style={{ display: "flex", alignItems: "baseline", marginTop: "10px" }}>
-				<p className="form-description">Import image :</p>
-				<div className="form-field">
-					<input
-						type="file"
-						name="avatar"
-						accept="image/*"
-						style={{ display: "none" }}
-						id="raised-button-file"
-					/>
-					<label htmlFor="raised-button-file">
-						<Button variant="contained" component="span">
-							Img, jpg ....
-						</Button>
-					</label>
-				</div>
-			</Box>
-			<Box
-				style={{ display: "flex", alignItems: "baseline", marginTop: "30px" }}>
-				<p className="form-description">Description :</p>
-				<div className="form-field">
-					<TextField
-						id="outlined-multiline-static"
-						label="Description"
-						multiline
-						rows={4}
-						size="medium"
-						sx={{ width: "400px !important" }}
-					/>
-				</div>
-			</Box>
-			<Box
-				style={{
-					display: "flex",
-					marginTop: "30px",
-					justifyContent: "flex-end",
-				}}>
-				<Button variant="contained">Apply</Button>
-			</Box>
-		</Box>
-	);
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		const {firstName, lastName, userDescription} = users;
+	}
+  return (
+    <Box
+      component="form"
+      sx={{
+        "& .MuiTextField-root": { m: 2, width: "25ch" },
+        ml: 5,
+      }}
+	  onSubmit={handleSubmit}
+    >
+      <h1 className="form-title">Profil</h1>
+      <Typography className="form-description">My profil :</Typography>
+      <Box sx={{display: "flex", flexDirection: "column", alignItems: "center"}}>
+        <Box sx={{display: "flex", alignItems: "baseline"}}>
+          <Typography>First Name :</Typography>
+          <TextField
+            id="outlined-basic"
+            name="firstName"
+            placeholder="First Name"
+            variant="outlined"
+			value={users.firstName}
+			onChange={handleChange}
+          />
+        </Box>
+        <Box sx={{display: "flex", alignItems: "baseline"}}>
+          <Typography>Last Name :</Typography>
+          <TextField
+            id="outlined-basic"
+            name="lastName"
+            placeholder="Last Name"
+            variant="outlined"
+			value={users.lastName}
+			onChange={handleChange}
+          />
+        </Box>
+      </Box>
+      <Box
+        style={{ display: "flex", alignItems: "baseline", marginTop: "10px" }}
+      >
+        <Typography className="form-description">Import image :</Typography>
+        <Box sx={{display: "flex", alignItems: "baseline", marginLeft: 5}}>
+          <input
+            type="file"
+            name="avatar"
+            accept="image/*"
+            style={{ display: "none" }}
+            id="raised-button-file"
+          />
+          <label htmlFor="raised-button-file">
+            <Button variant="contained" component="span">
+              Img, jpg ....
+            </Button>
+          </label>
+        </Box>
+      </Box>
+      <Box
+        style={{ display: "flex", alignItems: "baseline", marginTop: "30px" }}
+      >
+        <Typography className="form-description">Description :</Typography>
+        <Box  sx={{display: "flex", alignItems: "baseline", marginLeft: 5}}>
+          <TextField
+            id="outlined-multiline-static"
+            placeholder="Description"
+			name="userDescription"
+            multiline
+            rows={4}
+            size="medium"
+            sx={{ width: "400px !important" }}
+			onChange={handleChange}
+			value={users.userDescription}
+          />
+        </Box>
+      </Box>
+      <Box
+        style={{
+          display: "flex",
+          marginTop: "30px",
+          justifyContent: "flex-end",
+        }}
+      >
+        <Button type="submit" variant="contained" onSubmit={handleSubmit}>Apply</Button>
+      </Box>
+    </Box>
+  );
 }
