010b04e383680be4afc9640b5835c2815e274c67
Moymat <deckertmichel@gmail.com>
2021-11-04 14:40:31 +0100

    pre move and bristol's roles functions

--- server/app/models/bristolModel.js
+++ server/app/models/bristolModel.js
@@ -1,6 +1,23 @@
 const pgClient = require("../db/pg");
 const { validateBristol } = require("../validation");
 
+const createBristol = async (body, userId) => {
+	try {
+		const { data, errors } = await validateBristol(body);
+
+		if (errors) return { validationErrors: errors };
+
+		const { rows } = await pgClient.query(
+			"SELECT * FROM bristol.create_brisol($1)",
+			[JSON.stringify({ user_id: userId, ...data })]
+		);
+
+		return { data: rows[0] };
+	} catch (error) {
+		return { error };
+	}
+};
+
 const getBristol = async (bristolId, userId) => {
 	try {
 		const { rows } = await pgClient.query(
@@ -18,10 +35,52 @@ const getBristol = async (bristolId, userId) => {
 
 const moveBristol = async (bristolMoved, userId) => {
 	try {
-		return await pgClient.query("SELECT bristol.move_bristol($1)", [
+		const { rows } = await pgClient.query(
+			"SELECT * FROM bristol.bristol_pre_move($1)",
+			[JSON.stringify({ user_id: userId, bristol_id: bristolMoved.bristol_id })]
+		);
+
+		await pgClient.query("SELECT bristol.move_bristol($1)", [
 			JSON.stringify({
 				...bristolMoved,
 				user_id: userId,
+			}),
+		]);
+
+		const bristol_before = {
+			position: rows[0].position,
+			parent_id: rows[0].parent_id,
+		};
+
+		const roles = rows.map(({ user_id, role, first_name, last_name }) => ({
+			user_id,
+			role,
+			first_name,
+			last_name,
+		}));
+
+		return {
+			data: {
+				bristol_before,
+				editors: roles.filter(({ role }) => role === "editor"),
+				viewers: roles.filter(({ role }) => role === "viewer"),
+			},
+		};
+	} catch (error) {
+		return { error };
+	}
+};
+
+const patchBristol = async (body, userId) => {
+	try {
+		const { data, errors } = await validateBristol(body);
+
+		if (errors) return { validationErrors: errors };
+
+		return await pgClient("SELECT bristol.patch_bristol($1)", [
+			JSON.stringify({
+				userd_id: userId,
+				...data,
 			}),
 		]);
 	} catch (error) {
@@ -29,25 +88,9 @@ const moveBristol = async (bristolMoved, userId) => {
 	}
 };
 
-const createBristol = async (body, userId) => {
-	try {
-		const { data, errors } = await validateBristol(body);
-
-		if (errors) return { validationErrors: errors };
-
-		const { rows } = await pgClient.query(
-			"SELECT * FROM bristol.create_brisol($1)",
-			[JSON.stringify({ userId, ...data })]
-		);
-
-		return { data: rows[0] };
-	} catch (error) {
-		return { error };
-	}
-};
-
 module.exports = {
+	createBristol,
 	getBristol,
 	moveBristol,
-	createBristol,
+	patchBristol,
 };
