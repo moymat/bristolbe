27d8bc99aed17c136f93c56a9e6d93bd6f3a45f3
Moymat <deckertmichel@gmail.com>
2021-11-07 18:08:25 +0100

    fixed synthaxe error on 03 sqitch

--- server/migrations/revert/03_move_functions.sql
+++ server/migrations/revert/03_move_functions.sql
@@ -2,13 +2,13 @@
 
 BEGIN;
 
-DROP FUNCTION FUNCTION bristol.move_inside_root;
-DROP FUNCTION FUNCTION bristol.move_inside_bristol;
-DROP FUNCTION FUNCTION bristol.move_from_root;
-DROP FUNCTION FUNCTION bristol.move_to_root;
-DROP FUNCTION FUNCTION bristol.move_between_bristols;
-DROP FUNCTION FUNCTION bristol.move_bristol;
-DROP FUNCTION FUNCTION bristol.is_bristol_editor;
-DROP FUNCTION FUNCTION bristol.get_highest_parent;
+DROP FUNCTION bristol.move_inside_root;
+DROP FUNCTION bristol.move_inside_bristol;
+DROP FUNCTION bristol.move_from_root;
+DROP FUNCTION bristol.move_to_root;
+DROP FUNCTION bristol.move_between_bristols;
+DROP FUNCTION bristol.move_bristol;
+DROP FUNCTION bristol.is_bristol_editor;
+DROP FUNCTION bristol.get_highest_parent;
 
 COMMIT;
