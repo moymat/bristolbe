adbee280125b4adeb6410bdc0328d0683aeffe51
Moymat <deckertmichel@gmail.com>
2021-11-07 17:59:47 +0100

    reverts partial

--- server/migrations/revert/04_users_functions.sql
+++ server/migrations/revert/04_users_functions.sql
@@ -2,6 +2,13 @@
 
 BEGIN;
 
--- XXX Add DDLs here.
+DROP FUNCTION bristol.get_users_bristols;
+DROP FUNCTION bristol.delete_user;
+DROP FUNCTION bristol.patch_user_password;
+DROP FUNCTION bristol.patch_user_email;
+DROP FUNCTION bristol.patch_user_info;
+DROP FUNCTION bristol.get_user_auth;
+DROP FUNCTION bristol.get_user;
+DROP VIEW bristol.all_users;
 
 COMMIT;
