a4d77b7f9468573068b6101c4295e680c81f402e
Moymat <deckertmichel@gmail.com>
2021-11-09 21:07:21 +0100

    fixed alert options and name update error messages

--- client/src/views/User/Profile/index.js
+++ client/src/views/User/Profile/index.js
@@ -10,15 +10,21 @@ import axios from "../../../utils/axios";
 export default function Profile() {
 	const { user, setUser } = useContext(UserContext);
 	const [firstName, setFirstName] = useState(user.first_name);
+	const [firstNameError, setFirstNameError] = useState(false);
 	const [lastName, setLastName] = useState(user.last_name);
+	const [lastNameError, setLastNameError] = useState(false);
 	const [alertMessage, setAlertMessage] = useState("");
+	const [alertSeverity, setAlertSeverity] = useState("success");
 	const [isSnackOpen, setIsSnackOpen] = useState(false);
 
 	const handleChange = event => {
 		const { name, value } = event.target;
 		if (name === "firstName") {
+			console.log(!!value);
+			setFirstNameError(!value);
 			setFirstName(value);
 		} else if (name === "lastName") {
+			setLastNameError(!value);
 			setLastName(value);
 		}
 	};
@@ -26,7 +32,19 @@ export default function Profile() {
 	const handleSubmit = async e => {
 		e.preventDefault();
 
-		if (!firstName || !lastName) return;
+		if (firstName === user.first_name && lastName === user.last_name) {
+			setAlertSeverity("error");
+			setAlertMessage("Both first and last names haven't changed");
+			return setIsSnackOpen(true);
+		} else if (!firstName) {
+			setAlertSeverity("error");
+			setAlertMessage("A first name is required");
+			return setIsSnackOpen(true);
+		} else if (!lastName) {
+			setAlertSeverity("error");
+			setAlertMessage("A last name is required");
+			return setIsSnackOpen(true);
+		}
 
 		try {
 			await axios().patch(`/api/v1/users/${user.id}/info`, {
@@ -35,6 +53,7 @@ export default function Profile() {
 			});
 			setUser({ ...user, first_name: firstName, last_name: lastName });
 			setAlertMessage("Profile updated");
+			setAlertSeverity("success");
 			setIsSnackOpen(true);
 		} catch (error) {
 			console.error(error);
@@ -42,7 +61,6 @@ export default function Profile() {
 	};
 
 	const handleSnackClose = () => {
-		setAlertMessage("");
 		setIsSnackOpen(false);
 	};
 
@@ -58,7 +76,8 @@ export default function Profile() {
 			<CustomAlert
 				open={isSnackOpen}
 				handleClose={handleSnackClose}
-				options={{ message: alertMessage }}
+				message={alertMessage}
+				severity={alertSeverity}
 			/>
 			<Typography variant="h4" gutterBottom>
 				Profile
@@ -73,6 +92,8 @@ export default function Profile() {
 						variant="outlined"
 						value={firstName}
 						onChange={handleChange}
+						helperText={firstNameError ? "A first name is required" : ""}
+						error={firstNameError}
 					/>
 				</Box>
 				<Box sx={{ display: "flex", alignItems: "baseline" }}>
@@ -83,6 +104,8 @@ export default function Profile() {
 						variant="outlined"
 						value={lastName}
 						onChange={handleChange}
+						helperText={lastNameError ? "A last name is required" : ""}
+						error={lastNameError}
 					/>
 				</Box>
 			</Box>
