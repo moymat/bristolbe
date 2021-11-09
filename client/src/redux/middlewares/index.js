2c65bc33e977d26e99aa9a1674711934f3c2e388
Moymat <deckertmichel@gmail.com>
2021-11-09 17:08:02 +0100

    fixed update title and refactored redux calls for update and add new bristol

--- client/src/redux/middlewares/index.js
+++ client/src/redux/middlewares/index.js
@@ -1,41 +1,34 @@
 import axios from "../../utils/axios";
 import { createNestedMenu } from "../../components/Bristol/BristolTree/helper";
-import { v4 as uuidv4 } from "uuid";
-import { FlatMenu } from "../selectors/britols";
 
-const Middleware = (store) => (next) => (action) => {
-  switch (action.type) {
-    case "SAVE_UPDATE_EDITOR":
-      const state = store.getState();
-      if (!state.bristol.bristolId) {
-        axios()
-          .post(`/api/v1/bristols`, {
-            content: state.bristol.bristolContent,
-            title: state.bristol.bristolTitle,
-          })
-          .then(({ data }) => {
-            const { id } = data.data;
-            action.id = id
-            store.dispatch({
-              type: "ADD_NEW_BRISTOL", action
-            });
-            next(action);
-          })
-          .catch((err) => console.error("middleware", err));
-      } else {
-        axios()
-          .patch(`/api/v1/bristols/${state.bristol.bristolId}`, {
-            content: state.bristol.bristolContent,
-            title: state.bristol.bristolTitle,
-          })
-          .then(({ data }) => {
-            next(action);
-          })
-          .catch((err) => console.error("middleware", err.response.data));
-      }
-      break;
-    case "MOVE_BRISTOL":
-      axios()
+const Middleware = store => next => action => {
+	const state = store.getState();
+	switch (action.type) {
+		case "UPDATE_BRISTOL":
+			axios()
+				.patch(`/api/v1/bristols/${state.bristol.bristolId}`, {
+					content: state.bristol.bristolContent,
+					title: state.bristol.bristolTitle,
+				})
+				.then(() => {
+					next(action);
+				})
+				.catch(err => console.error("middleware", err.response.data));
+			break;
+		case "ADD_NEW_BRISTOL":
+			axios()
+				.post(`/api/v1/bristols`, {
+					content: state.bristol.bristolContent,
+					title: state.bristol.bristolTitle,
+				})
+				.then(({ data }) => {
+					action.id = data.data.id;
+					next(action);
+				})
+				.catch(err => console.error("middleware", err));
+			break;
+		case "MOVE_BRISTOL":
+			axios()
 				.post("/api/v1/bristols/move", {
 					bristol_id: action.id,
 					parent_id: action.parent_id,
@@ -45,29 +38,29 @@ const Middleware = (store) => (next) => (action) => {
 					next(action);
 				})
 				.catch(err => console.error("middleware", err));
-      break;
-    case "SET_BRISTOLS":
-      axios()
-        .get(`api/v1/users/${action.userId}/bristols`)
-        .then(({ data }) => {
-          action.bristols = createNestedMenu(data.data);
-          next(action);
-        })
-        .catch((err) => console.error(err));
-      break;
-    case "GET_CURRENT_BRISTOL_CONTENT":
-      axios()
-        .get(`/api/v1/bristols/${action.selectedBristol}`)
-        .then(({ data }) => {
-          action.data = data.data
-          next(action);
-        })
-        .catch((err) => console.error(err));
-      break;
-    default:
-      next(action);
-      break;
-  }
+			break;
+		case "SET_BRISTOLS":
+			axios()
+				.get(`api/v1/users/${action.userId}/bristols`)
+				.then(({ data }) => {
+					action.bristols = createNestedMenu(data.data);
+					next(action);
+				})
+				.catch(err => console.error(err));
+			break;
+		case "GET_CURRENT_BRISTOL_CONTENT":
+			axios()
+				.get(`/api/v1/bristols/${action.selectedBristol}`)
+				.then(({ data }) => {
+					action.data = data.data;
+					next(action);
+				})
+				.catch(err => console.error(err));
+			break;
+		default:
+			next(action);
+			break;
+	}
 };
 
 export default Middleware;
--- client/src/redux/reducers/bristol/index.js
+++ client/src/redux/reducers/bristol/index.js
@@ -1,141 +1,149 @@
 const initialState = {
-  //editor state
-  editorIsVisible: false,
-  editorIsReadOnly: true,
-  //current Bristol
-  bristolCurrentUserIsEditor: "reader",
-  bristolId: null,
-  bristolContent: "",
-  bristolTitle: "",
-  bristolParentId: null,
-  bristolPositionId: null,
-  bristolEditorsList: [],
-  bristolReadersList: [],
-  //utility
-  usersSearchList: [],
-  //tree
-  bristols: [],
-  movedBristol: {
-    id: null,
-    parent_id: null,
-    position: null,
-  },
+	//editor state
+	editorIsVisible: false,
+	editorIsReadOnly: true,
+	//current Bristol
+	bristolCurrentUserIsEditor: "reader",
+	bristolId: null,
+	bristolContent: "",
+	bristolTitle: "",
+	bristolParentId: null,
+	bristolPositionId: null,
+	bristolEditorsList: [],
+	bristolReadersList: [],
+	//utility
+	usersSearchList: [],
+	//tree
+	bristols: [],
+	movedBristol: {
+		id: null,
+		parent_id: null,
+		position: null,
+	},
 };
 
-const updateTitle = (arr, id, newTitle) => {
-  return arr.map(bristol => {
-    if (bristol.children) {bristol.children = updateTitle(bristol.children, id, newTitle)}
-    return bristol.id === id ? {...bristol, title: newTitle} : bristol
-  })
-}
+const updateTitle = (arr, id, newTitle) =>
+	arr.map(bristol => {
+		if (bristol.children)
+			bristol.children = updateTitle(bristol.children, id, newTitle);
+		return bristol.id === id ? { ...bristol, title: newTitle } : bristol;
+	});
 
 const reducer = (state = initialState, action = {}) => {
-  switch (action.type) {
-    case "CREATE_NEW_BRISTOL":
-      return {
-        ...state,
-        editorIsVisible: true,
-        editorIsReadOnly: false,
-        bristolCurrentUserIsEditor: "editor",
-        bristolId: null,
-        bristolContent: "",
-        bristolTitle: "",
-        bristolParentId: null,
-        bristolEditorsList: [],
-        bristolReadersList: [],
-      };
-    case "GET_CURRENT_BRISTOL_CONTENT":
-      return {
-        ...state,
-        editorIsVisible: true,
-        editorIsReadOnly: true,
-        bristolId: action.data.id,
-        bristolTitle: action.data.title,
-        bristolContent: action.data.content,
-        bristolCurrentUserIsEditor: action.data.role,
-        bristolPositionId: action.data.position,
-        bristolParentId: action.data.parent_id,
-        bristolEditorsList: [],
-        bristolReadersList: [],
-      };
-    case "EDIT_CURRENT_BRISTOL":
-      return {
-        ...state,
-        editorIsVisible: true,
-        editorIsReadOnly: false,
-        bristolCurrentUserIsEditor: "editor",
-      };
-    case "UPDATE_BRISTOL_TITLE":
-      return {
-        ...state,
-        bristolTitle: action.bristolTitle,
-      };
-    case "UPDATE_BRISTOL_CONTENT":
-      return {
-        ...state,
-        bristolContent: action.bristolContent,
-      };
-    case "UPDATE_BRISTOL_EDITORS":
-      return {
-        ...state,
-        bristolEditorsList: action.bristolEditorsList,
-      };
-    case "UPDATE_BRISTOL_READERS":
-      return {
-        ...state,
-        bristolReadersList: action.bristolReadersList,
-      };
-    case "CANCEL_UPDATE_EDITOR":
-      return {
-        ...state,
-        editorIsVisible: false,
-        editorIsReadOnly: true,
-        bristolCurrentUserIsEditor: "reader",
-        bristolId: null,
-        bristolContent: "",
-        bristolTitle: "",
-        bristolParentId: null,
-        bristolPositionId: null,
-        bristolEditorsList: [],
-        bristolReadersList: [],
-      };
-    case "SAVE_UPDATE_EDITOR":
-      console.log(state.bristols)
-      return {
-        ...state,
-        bristols: updateTitle(state.bristols, state.bristolId, state.bristolTitle),
-        editorIsVisible: true,
-        editorIsReadOnly: true,
-        bristolCurrentUserIsEditor: "editor",
-      };
-    case "ADD_NEW_BRISTOL":
-      console.log(state.bristols)
-      return {
-        ...state,
-        bristols: state.bristols.push({
-          id: action.id,
-          title: state.bristolTitle,
-          parent_id: null,
-          position: state.bristols.length,
-          role: "editor",
-          editors: [],
-          readers: [],
-        })
-      };
-    case "SET_BRISTOLS":
-      return {
-        ...state,
-        bristols: action.bristols,
-      };
-    case "MOVE_BRISTOL":
-      console.log(action.items)
-      return {
-        ...state,
-        bristols: action.items,
-      };
-    default:
-      return state;
-  }
+	switch (action.type) {
+		case "CREATE_NEW_BRISTOL":
+			return {
+				...state,
+				editorIsVisible: true,
+				editorIsReadOnly: false,
+				bristolCurrentUserIsEditor: "editor",
+				bristolId: null,
+				bristolContent: "",
+				bristolTitle: "",
+				bristolParentId: null,
+				bristolEditorsList: [],
+				bristolReadersList: [],
+			};
+		case "GET_CURRENT_BRISTOL_CONTENT":
+			return {
+				...state,
+				editorIsVisible: true,
+				editorIsReadOnly: true,
+				bristolId: action.data.id,
+				bristolTitle: action.data.title,
+				bristolContent: action.data.content,
+				bristolCurrentUserIsEditor: action.data.role,
+				bristolPositionId: action.data.position,
+				bristolParentId: action.data.parent_id,
+				bristolEditorsList: [],
+				bristolReadersList: [],
+			};
+		case "EDIT_CURRENT_BRISTOL":
+			return {
+				...state,
+				editorIsVisible: true,
+				editorIsReadOnly: false,
+				bristolCurrentUserIsEditor: "editor",
+			};
+		case "UPDATE_BRISTOL_TITLE":
+			return {
+				...state,
+				bristolTitle: action.bristolTitle,
+			};
+		case "UPDATE_BRISTOL_CONTENT":
+			return {
+				...state,
+				bristolContent: action.bristolContent,
+			};
+		case "UPDATE_BRISTOL_EDITORS":
+			return {
+				...state,
+				bristolEditorsList: action.bristolEditorsList,
+			};
+		case "UPDATE_BRISTOL_READERS":
+			return {
+				...state,
+				bristolReadersList: action.bristolReadersList,
+			};
+		case "CANCEL_UPDATE_EDITOR":
+			return {
+				...state,
+				editorIsVisible: false,
+				editorIsReadOnly: true,
+				bristolCurrentUserIsEditor: "reader",
+				bristolId: null,
+				bristolContent: "",
+				bristolTitle: "",
+				bristolParentId: null,
+				bristolPositionId: null,
+				bristolEditorsList: [],
+				bristolReadersList: [],
+			};
+		case "UPDATE_BRISTOL":
+			return {
+				...state,
+				bristols: updateTitle(
+					state.bristols,
+					state.bristolId,
+					state.bristolTitle
+				),
+				editorIsVisible: true,
+				editorIsReadOnly: true,
+				bristolCurrentUserIsEditor: "editor",
+			};
+		case "ADD_NEW_BRISTOL":
+			return {
+				...state,
+				bristols: [
+					...state.bristols,
+					{
+						id: action.id,
+						title: state.bristolTitle,
+						parent_id: null,
+						position: state.bristols.length,
+						role: "editor",
+						editors: [],
+						readers: [],
+					},
+				],
+				editorIsVisible: true,
+				editorIsReadOnly: true,
+				bristolCurrentUserIsEditor: "editor",
+			};
+		case "SET_BRISTOLS":
+			return {
+				...state,
+				bristols: action.bristols,
+			};
+		case "MOVE_BRISTOL":
+			console.log(action.items);
+			return {
+				...state,
+				bristols: action.items,
+			};
+		default:
+			return state;
+	}
 };
 
 export default reducer;
