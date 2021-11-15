55cb5ecd9863f523893b99d02bae37f2891d4555
Moymat <deckertmichel@gmail.com>
2021-11-15 23:18:16 +0100

    fixed huge nb of socket calls

--- server/app/models/socketIOModel.js
+++ server/app/models/socketIOModel.js
@@ -5,7 +5,6 @@ const {
 } = require("../socketio");
 
 const onConnection = async (socketId, userId) => {
-	console.log("connection", socketId, userId);
 	await redisClient("socket_id_").setAsync(userId, socketId);
 };
 
@@ -21,18 +20,17 @@ const onEditing = async (socket, bristolId) => {
 		.emit("in_editing", { bristolId, userId });
 };
 
-const onStopEditing = async (socket, bristolId) => {
-	await redisClient("in_editing_").delAsync(bristolId);
-	socket.broadcast.to(`bristol_${bristolId}`).emit("stop_editing", bristolId);
+const onStopEditing = async (socket, args) => {
+	await redisClient("in_editing_").delAsync(args.bristolId);
+	socket.broadcast.to(`bristol_${args.bristolId}`).emit("stop_editing", args);
 };
 
 const onMoved = async (socket, bristolId) => {
-	socket.broadcast.to(`bristol_${bristolId}`).emit("moved", bristolId);
+	socket.broadcast.to(`bristol_${bristolId}`).emit("moved");
 };
 
 const onRolesManaged = async (socket, { bristolId, roles }) => {
 	Object.entries(roles).forEach(([key, ids]) => {
-		console.log(key, ids);
 		key === "editors_id" || key === "viewers_id"
 			? connectSocketsToBristols(ids, [bristolId])
 			: disconnectSocketsFromBristols(ids, [bristolId]);
@@ -41,28 +39,25 @@ const onRolesManaged = async (socket, { bristolId, roles }) => {
 };
 
 const onDeleted = async (socket, bristolId) => {
-	socket.broadcast.to(`bristol_${bristolId}`).emit("deleted", bristolId);
+	socket.broadcast.to(`bristol_${bristolId}`).emit("deleted", { bristolId });
 };
 
 const onDisconnect = async socket => {
+	const userId = socket.handshake.query.id;
+
 	await redisClient("socket_id_").delAsync(socket.handshake.query.id);
 
 	const allInEditing = await redisClient().keysAsync("in_editing_*");
 	await Promise.all(
 		allInEditing.map(async key => {
 			const bristolId = key.replace("in_editing_", "");
+			const editorId = await redisClient("in_editing_").getAsync(bristolId);
 
-			const userId = await redisClient("in_editing_").getAsync(bristolId);
-
-			if (userId) {
-				const cachedId = await redisClient("socket_id_").getAsync(userId);
-
-				if (socket.id === cachedId) {
-					await redisClient("in_editing_").delAsync(bristolId);
-					socket.broadcast
-						.to(`bristol_${bristolId}`)
-						.emit("stop_editing", { bristolId });
-				}
+			if (editorId === userId) {
+				await redisClient("in_editing_").delAsync(bristolId);
+				socket.broadcast
+					.to(`bristol_${bristolId}`)
+					.emit("stop_editing", { bristolId });
 			}
 		})
 	);
