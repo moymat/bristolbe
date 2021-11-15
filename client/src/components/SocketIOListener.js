ce71f566927c74483aa73c744a4ca3bc7eca04c3
Moymat <deckertmichel@gmail.com>
2021-11-15 23:18:16 +0100

    fixed huge nb of socket calls

--- client/src/components/SocketIOListener.js
+++ client/src/components/SocketIOListener.js
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 
 const SocketIOListener = ({ children }) => {
@@ -5,31 +6,35 @@ const SocketIOListener = ({ children }) => {
 	const selectedBristol = useSelector(state => state.bristol.selectedBristol);
 	const dispatch = useDispatch();
 
-	user.socket.on("in_editing", data => {
-		dispatch({ type: "SIO_SET_EDITING", data });
-	});
+	useEffect(() => {
+		user.socket.on("in_editing", data => {
+			dispatch({ type: "SIO_SET_EDITING", data });
+		});
 
-	user.socket.on("stop_editing", data => {
-		dispatch({ type: "SIO_UNSET_EDITING", data });
-		selectedBristol.id === data.bristolId &&
-			data.didContentChanged &&
-			dispatch({
-				type: "GET_CURRENT_BRISTOL_CONTENT",
-				selectedBristol: data.bristolId,
-			});
-	});
+		user.socket.on("stop_editing", data => {
+			dispatch({ type: "SIO_UNSET_EDITING", data });
+			selectedBristol.id === data.bristolId &&
+				data.didContentChanged &&
+				dispatch({
+					type: "GET_CURRENT_BRISTOL_CONTENT",
+					selectedBristol: data.bristolId,
+				});
+		});
 
-	user.socket.on("moved", data => {
-		dispatch({ type: "SET_BRISTOLS", data });
-	});
+		user.socket.on("moved", () => {
+			dispatch({ type: "SET_BRISTOLS" });
+		});
 
-	user.socket.on("roles_managed", () => {
-		dispatch({ type: "SET_BRISTOLS" });
-	});
+		user.socket.on("roles_managed", () => {
+			dispatch({ type: "SET_BRISTOLS" });
+		});
 
-	user.socket.on("deleted", data => {
-		dispatch({ type: "SIO_DELETED", data });
-	});
+		user.socket.on("deleted", data => {
+			dispatch({ type: "SIO_DELETED", data });
+		});
+
+		return () => user.socket.removeAllListeners();
+	}, [user, selectedBristol, dispatch]);
 
 	return <>{children}</>;
 };
