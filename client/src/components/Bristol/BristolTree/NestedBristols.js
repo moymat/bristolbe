fb477938b8d0b6bbe7af69dbbcde7916af55b176
Moymat <deckertmichel@gmail.com>
2021-11-15 13:14:08 +0100

    removed right management for non-root bristols

--- client/src/components/Bristol/BristolTree/NestedBristols.js
+++ client/src/components/Bristol/BristolTree/NestedBristols.js
@@ -1,7 +1,6 @@
 import { useEffect, useRef } from "react";
 import Nestable from "react-nestable";
 import NestedItem from "./NestedItem";
-import Box from "@mui/material/Box";
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
 import "./nestedStyles.css";
@@ -32,26 +31,24 @@ const NestedBristols = ({ handleItemMove = null, items, handleConfirm }) => {
 	}, []);
 
 	return (
-		<Box>
-			<Nestable
-				groupe={1}
-				collapsed={true}
-				maxDepth={15}
-				items={items}
-				renderItem={({ item, collapseIcon }) => (
-					<NestedItem item={item} collapseIcon={collapseIcon} key={item.id} />
-				)}
-				onChange={handleItemMove}
-				confirmChange={handleConfirm}
-				renderCollapseIcon={({ isCollapsed }) =>
-					isCollapsed ? (
-						<KeyboardArrowRightIcon fontSize="small" />
-					) : (
-						<KeyboardArrowDownIcon fontSize="small" />
-					)
-				}
-			/>
-		</Box>
+		<Nestable
+			groupe={1}
+			collapsed={true}
+			maxDepth={15}
+			items={items}
+			renderItem={({ item, collapseIcon }) => (
+				<NestedItem item={item} collapseIcon={collapseIcon} key={item.id} />
+			)}
+			onChange={handleItemMove}
+			confirmChange={handleConfirm}
+			renderCollapseIcon={({ isCollapsed }) =>
+				isCollapsed ? (
+					<KeyboardArrowRightIcon fontSize="small" />
+				) : (
+					<KeyboardArrowDownIcon fontSize="small" />
+				)
+			}
+		/>
 	);
 };
 
