38c2813d0daae56739115cb02e529697d0912ad0
Moymat <deckertmichel@gmail.com>
2021-11-16 19:03:46 +0100

    redis connect_timeout: 15000

--- server/app/db/redis.js
+++ server/app/db/redis.js
@@ -7,14 +7,14 @@ const redisClient = (prefix = "refresh_token_") => {
 	process.env.NODE_ENV === "production" &&
 		(options.url = process.env.REDIS_URL);
 
-	const client = redis.createClient({ ...options });
+	const client = redis.createClient({ ...options, connect_timeout: 15000 });
 
 	const getAsync = promisify(client.get).bind(client);
 	const setAsync = promisify(client.set).bind(client);
 	const delAsync = promisify(client.del).bind(client);
 	const keysAsync = promisify(client.keys).bind(client);
 
-	return { getAsync, setAsync, delAsync, keysAsync };
+	return { getAsync, setAsync, delAsync, keysAsync, client };
 };
 
 module.exports = redisClient;
