f1e9ca5b110a839ba3ee3850f5b3ccc8a0f4e2a1
Moymat <deckertmichel@gmail.com>
2021-11-17 11:18:34 +0100

    fixed alert

--- client/src/components/CustomAlert.js
+++ client/src/components/CustomAlert.js
@@ -1,6 +1,7 @@
 import Snackbar from "@mui/material/Snackbar";
 import Alert from "@mui/material/Alert";
 import Slide from "@mui/material/Slide";
+import useMediaQuery from "@mui/material/useMediaQuery";
 
 const CustomAlert = ({
 	open,
@@ -9,8 +10,11 @@ const CustomAlert = ({
 	duration = 3000,
 	severity = "success",
 }) => {
+	const isSmallScreen = useMediaQuery(theme => theme.breakpoints.down("sm"));
+
 	return (
 		<Snackbar
+			sx={{ top: isSmallScreen ? 60 : 8 }}
 			open={open}
 			anchorOrigin={{ vertical: "top", horizontal: "center" }}
 			autoHideDuration={duration}
