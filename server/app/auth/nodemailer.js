5d1a39729212067679b56e529212fe24f0429bf5
Moymat <deckertmichel@gmail.com>
2022-01-22 00:08:09 +0100

    added TLS nodemailer

--- server/app/auth/nodemailer.js
+++ server/app/auth/nodemailer.js
@@ -6,6 +6,7 @@ const transporter = nodemailer.createTransport({
 	host: process.env.NODEMAILER_HOST,
 	port: process.env.NODEMAILER_PORT,
 	secure: false,
+	requireTLS: true,
 	auth: {
 		user: process.env.NODEMAILER_USER,
 		pass: process.env.NODEMAILER_PASS,
